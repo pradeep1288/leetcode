c9ee7a90d8e801e5543f0afd999ca128b39445e4
Pradeep Nayak <pradeep1288@gmail.com>
2016-07-21 20:42:17 -0700

    serialize deserialize binary tree

---
+++ hard/serialize-and-deserialize-binary-tree/c++/serialize-and-deserialize-binary-tree.cpp
@@ -0,0 +1,49 @@
+class Codec {
+public:
+    /**
+     * This method will be invoked first, you should design your own algorithm
+     * to serialize a binary tree which denote by a root node to a string which
+     * can be easily deserialized by your own "deserialize" method later.
+     */
+    string serialize(TreeNode *root) {
+        ostringstream out;
+        serializeHelper(root, out);
+        return out.str();
+    }
+
+    /**
+     * This method will be invoked second, the argument data is what exactly
+     * you serialized at method "serialize", that means the data is not given by
+     * system, it's given by your own serialize method. So the format of data is
+     * designed by yourself, and deserialize it here as you serialize it in
+     * "serialize" method.
+     */
+    TreeNode *deserialize(string data) {
+        istringstream in(data);
+        return deserializeHelper(in);
+    }
+
+private:
+    void serializeHelper(const TreeNode *root, ostringstream& out) {
+        if (!root)  {
+            out << "# ";
+        } else {
+            out << root->val << " ";
+            serializeHelper(root->left, out);
+            serializeHelper(root->right, out);
+        }
+    }
+
+    TreeNode *deserializeHelper(istringstream& in) {
+        string val;
+        in >> val;
+        if (val == "#") {
+            return nullptr;
+        } else {
+            TreeNode* root = new TreeNode(stoi(val));
+            root->left = deserializeHelper(in);
+            root->right = deserializeHelper(in);
+            return root;
+        }
+    }
+};
